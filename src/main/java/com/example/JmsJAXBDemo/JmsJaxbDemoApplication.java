f8fd47be595c05d1a8ce834c8775673d3186cf86
Yassine DIRAH <yassinedirah@gmail.com>
2025-11-08 19:36:57 +0100

    Initializing spring boot project

---
+++ src/main/java/com/example/JmsJAXBDemo/JmsJaxbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.JmsJAXBDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsJaxbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsJaxbDemoApplication.class, args);
+	}
+
+}
